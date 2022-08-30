132ee587ed81a7d5b088e772f38d4cdb1731d90e
Bruno Medeiros <brunojcm@gmail.com>
2022-08-30 22:17:09 +0800

    feat(ktor): add ktor serialization support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 group = "dev.bcmedeiros"
@@ -14,4 +15,6 @@ dependencies {
 
     implementation("io.ktor:ktor-client-core:2.1.0")
     implementation("io.ktor:ktor-client-java:2.1.0")
+    implementation("io.ktor:ktor-client-content-negotiation:2.1.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.0")
 }
