a9f27fc52a61a397820998aaeae49ec9bd5b215a
Bruno Medeiros <brunojcm@gmail.com>
2022-09-21 02:05:09 +0800

    feat(ktor): upgrade to 2.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,10 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("io.ktor:ktor-client-core:2.1.0")
-    implementation("io.ktor:ktor-client-java:2.1.0")
-    implementation("io.ktor:ktor-client-content-negotiation:2.1.0")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.0")
+    implementation("io.ktor:ktor-client-core:2.1.1")
+    implementation("io.ktor:ktor-client-java:2.1.1")
+    implementation("io.ktor:ktor-client-content-negotiation:2.1.1")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.1")
 
     testImplementation(kotlin("test"))
 }
